59cb4a42d66ec86f4f48f268382cb9f5b29ac1cb
vumt2 <vumt2@vcu.edu>
2017-05-04 08:55:28 -0400

    changed the format to match our project

--- sql/product_procedure.sql
+++ sql/product_procedure.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE DEFINER=`cmsc508`@`localhost` PROCEDURE `add_product`(IN `code` INT(11), IN `name` VARCHAR(50), IN `type` VARCHAR(50), IN `amount` DECIMAL(6,0), IN `price` DECIMAL(6,0))
+CREATE OR REPLACE PROCEDURE add_product(IN code INT, IN name VARCHAR(50), IN type VARCHAR(50), IN amount DECIMAL(6,0), IN price DECIMAL(6,0))
     MODIFIES SQL DATA
 BEGIN
 INSERT INTO product VALUES (code, name, type, amount, price);
@@ -7,7 +7,7 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`cmsc508`@`localhost` PROCEDURE `view_products`()
+CREATE OR REPLACE PROCEDURE view_products()
     READS SQL DATA
 BEGIN
 SELECT * FROM product;
@@ -15,7 +15,7 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`cmsc508`@`localhost` PROCEDURE `delete_product`(IN `product_code` INT(11))
+CREATE OR REPLACE PROCEDURE delete_product(IN product_code INT)
     MODIFIES SQL DATA
 BEGIN
 DELETE FROM product WHERE product.product_code = product_code;
@@ -23,7 +23,7 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`cmsc508`@`localhost` PROCEDURE `edit_product`(IN `product_code` INT(11), IN `name` VARCHAR(50), IN `type` VARCHAR(50), IN `amount` DECIMAL(6,0), IN `price` DECIMAL(6,0))
+CREATE OR REPLACE PROCEDURE edit_product(IN product_code INT, IN name VARCHAR(50), IN type VARCHAR(50), IN amount DECIMAL(6,0), IN price DECIMAL(6,0))
     MODIFIES SQL DATA
 BEGIN
 UPDATE product set product.name = name, product.type = type, product.amount = amount, product.price = price where product.product_code =product_code;
